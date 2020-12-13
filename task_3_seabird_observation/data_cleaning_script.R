fbd0836c6207e996662d8f068f480ddf829f3f5a
laurenausoleil <lauren.au.soleil@gmail.com>
2020-12-13 21:43:07 +0000

    Completed data clean

--- task_3_seabird_observation/data_cleaning_script.R
+++ task_3_seabird_observation/data_cleaning_script.R
@@ -1,11 +1,61 @@
+# --------------
+# Loading in data
+# --------------
+
 library(tidyverse)
 library(readxl)
-
-excel_sheets("raw_data/seabirds.xls")
+library(janitor)
+library(here)
 
 ship_data <- read_excel("raw_data/seabirds.xls", sheet = "Ship data by record ID")
 bird_data <- read_excel("raw_data/seabirds.xls", sheet = "Bird data by record ID")
-# Expecting logical in I21756 / R21756C9: got 'M'
+# If reading in, please ignore error message as it is not relevant to the analysis questions
 
-ship_codes <- read_excel("raw_data/seabirds.xls", sheet = "Ship data codes")
-bird_codes <- read_excel("raw_data/seabirds.xls", sheet = "Bird data codes")
+# --------------
+# Removing excess variables
+# --------------
+# Reduce to only the variables needed for the analysis questions.
+
+bird_data <- bird_data %>% 
+  select(
+    "RECORD ID",
+    "Species common name (taxon [AGE / SEX / PLUMAGE PHASE])",
+    "Species  scientific name (taxon [AGE /SEX /  PLUMAGE PHASE])",
+    "Species abbreviation",
+    "COUNT"
+  )
+
+ship_data <- ship_data %>% 
+  select(
+    LAT,
+    `RECORD ID`
+  )
+
+# --------------
+# Joining tables
+# --------------
+# Joining the 2 variable version of ship_data to bird_data. A left join keeps all the records from bird data allowing us to answer count questions more fully. We add the column latitude to answer the question: "Which bird had the highest total count above a latitude of -30?" set in the task.
+
+bird_data <- bird_data %>% 
+  left_join(ship_data, by = "RECORD ID", keep = FALSE)
+
+
+# --------------
+# Cleaning variable names for best practice and readability
+# --------------
+
+bird_data <- bird_data %>% 
+  clean_names() %>% 
+  rename(
+    "id" = "record_id",
+    "common_name" = "species_common_name_taxon_age_sex_plumage_phase",
+    "scientific_name" = "species_scientific_name_taxon_age_sex_plumage_phase",
+    "latitude" = "lat"
+  )
+
+# --------------
+# Writing cleaned csv
+# --------------
+
+bird_data %>% 
+  write_csv(here("clean_data/seabird_data_clean"))
